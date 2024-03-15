a8129070452823dc052eeae80aa55f9ddd21c36d
Akash Mondal <96812236+akash-mondal@users.noreply.github.com>
2024-03-15 22:03:14 +0530

    Add files via upload

---
+++ mixtral.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+url = "https://api.fireworks.ai/inference/v1/chat/completions"
+payload = {
+  "model": "accounts/fireworks/models/mistral-7b-instruct-4k",
+  "max_tokens": 4096,
+  "top_p": 1,
+  "top_k": 40,
+  "presence_penalty": 0,
+  "frequency_penalty": 0,
+  "temperature": 0.6,
+  "messages": [
+    {
+      "role": "user",
+      "content": "hi"
+    }
+  ]
+}
+headers = {
+  "Accept": "application/json",
+  "Content-Type": "application/json",
+  "Authorization": "Bearer <API_KEY>"
+}
+response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+llm_response = response.json()['choices'][0]['message']['content'].replace('\n', '')
+print(llm_response)
